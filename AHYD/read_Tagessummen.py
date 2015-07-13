8543502edf18e4e61a22b66e8f76daaf3c6c3d70
KSpunkt <Katha.Spunkt@gmail.com>
2015-07-13 11:45:26 +0200

    AHYD Tagessummen with correct index
    removed erroneous dates

--- AHYD/read_Tagessummen.py
+++ AHYD/read_Tagessummen.py
@@ -88,57 +88,90 @@ for eachElement in statlist_AHYD:
     if eachElement not in success_stations:
         fail_stations.append(eachElement)
 
-df_ahyd_all.to_pickle(src + '\AHYD_dailysums.npy')
+df_ahyd_all.to_pickle(src + '\AHYD_dailysums_raw.npy')
+
+
 # -----------------------------------
 # ----------------- Repair dataframe (edited 2015-07-10)
 # -----------------------------------
 
-df_ahyd_all = pd.read_pickle(src + '\AHYD_dailysums.npy')
+df_ahyd_all_raw = pd.read_pickle(src + '\AHYD_dailysums_raw.npy')
 
-# there are some erroneous data points, where one day has more than one 
-# observation
+'''
+there are some erroneous data points, where one day has more than one 
+observation'''
 
 # first and last day of observations in file
-start = df_ahyd_all.index[0]
-end = df_ahyd_all.index[-1]
+start = df_ahyd_all_raw.index[0]
+end = df_ahyd_all_raw.index[-1]
 
 # span daily range between first and last day
 correct_daily_range = pd.date_range(start, end, freq='1D')
 
 # find number of errors (=additional days)
-errors = len(df_ahyd_all.index) - len(correct_daily_range)
+errors = len(df_ahyd_all_raw.index) - len(correct_daily_range)
 
 # find strange datetime indices (should be 07:00:00 for all)
-possible_errors_sec = np.where(df_ahyd_all.index.second!=0)[0]
-possible_errors_min = np.where(df_ahyd_all.index.minute!=0)[0]
-possible_errors_hour = np.where(df_ahyd_all.index.hour!=7)[0]
+possible_errors_sec = np.where(df_ahyd_all_raw.index.second!=0)[0]
+possible_errors_min = np.where(df_ahyd_all_raw.index.minute!=0)[0]
+possible_errors_hour = np.where(df_ahyd_all_raw.index.hour!=7)[0]
 
 # indices of strange dates
 ind_err = np.unique(np.concatenate((possible_errors_hour,
                                        possible_errors_min,
                                        possible_errors_sec), axis=0))                               
-strange_dates = df_ahyd_all.index[ind_err] 
+strange_dates = df_ahyd_all_raw.index[ind_err] 
 
 # Dataframe with all weird times in it:
 dates_list = []
 for ID in range(len(strange_dates)):
-    entry = df_ahyd_all[str(strange_dates[ID])]
+    entry = df_ahyd_all_raw[str(strange_dates[ID])]
     dates_list.append(entry)    
 all_dates_errors = pd.concat(dates_list, axis=0, join='outer') 
 
 # drop rows where all values are NAN                                  
 all_dates_errors_not_NAN = all_dates_errors.dropna(how='all')
 
-# gives for each date the station that had the highest value for the day
+# gives the station that had the highest value for the day for each date 
 all_dates_errors_not_NAN.idxmax(axis=1).dropna(how='all')
 
 # for each station the date where the station had its max
 all_dates_errors_not_NAN.idxmax(axis=0).dropna(how='all')
 
-# replace the 07:00:00 values with the 07:30:00
-df_ahyd_all.ix['1998-05-01 07:00:00', '112730'] = np.float(df_ahyd_all['112730']['1998-05-01 07:30:00'])
+''' 
+* 34 observations are shifted by max 30 min (07:30 to 07:00)
+* 8 observations are deleted, i.e. set to missing value (2 of them were 0 at
+00:00 and 17:26:45 and 6 were missing values at strange times)
+* for Station 112730 May 1998'''
 
+for day in range(1,31):
+    df_ahyd_all_raw.ix['1998-05-'+ str(day) + ' 07:00:00', '112730'
+                   ] = np.float(df_ahyd_all_raw['112730']['1998-05-'+ str(day)+
+                                                      ' 07:30:00'])
+# rewrite values to 7:000:00 when time is close to 7am                                                       
+df_ahyd_all_raw.ix['1998-03-01 07:00:00', '111310'
+               ] = np.float(df_ahyd_all_raw['111310']['1998-03-01 07:30:00'])
+df_ahyd_all_raw.ix['1998-03-02 07:00:00', '114868'
+               ] = np.float(df_ahyd_all_raw['114868']['1998-03-02 06:59:55'])
+df_ahyd_all_raw.ix['2008-07-17 07:00:00', '111476'
+               ] = np.float(df_ahyd_all_raw['111476']['2008-07-17 07:00:05'])
+df_ahyd_all_raw.ix['2009-09-25 07:00:00', '113803'
+               ] = np.float(df_ahyd_all_raw['113803']['2009-12-25 07:00:05'])
 
+# delete the rows where the time is not 07:00:00
+df_ahyd_all_raw = df_ahyd_all_raw[df_ahyd_all_raw.index.hour == 7]
+df_ahyd_all_raw = df_ahyd_all_raw[df_ahyd_all_raw.index.minute == 0]
+df_ahyd_all_raw = df_ahyd_all_raw[df_ahyd_all_raw.index.second == 0]
+
+# test
+errors_test = len(df_ahyd_all_raw.index) - len(correct_daily_range)
+
+if errors_test==0:
+    df_ahyd_all_raw.index = correct_daily_range
+    df_ahyd_all_raw.to_pickle(src + '\AHYD_dailysums.npy')
+else:
+    print 'Error! Check for inconsistencies'
+    
 
 
 
